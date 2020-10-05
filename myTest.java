15608ffe9e75de79ff239cd7f864959ef1f80790
sreenidhi27s <72385988+sreenidhi27s@users.noreply.github.com>
2020-10-05 16:31:48 +0530

    Update myTest.java

--- myTest.java
+++ myTest.java
@@ -16,24 +16,26 @@ public class myTest
     int A;
     int B;
     int C;
+    int D;
     
     System.out.println("Please enter 3 numbers: ");
     A = scan.nextInt();
     B = scan.nextInt();
     C = scan.nextInt();
+    D = scan.nextInt();
     
-    int add = getData(A, B, C);
+    int add = getData(A, B, C, D);
     
     System.out.println("The total sum is: " + add);
     
         
   }
   
-  public static int getData(int num1, int num2, int num3)
+  public static int getData(int num1, int num2, int num3,int num4)
   {
     int total;
     
-    total = num1 + num2 + num3;
+    total = num1 + num2 + num3+num4;
     
     return total;
   }
